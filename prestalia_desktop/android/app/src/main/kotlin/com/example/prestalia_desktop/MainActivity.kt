7cbb16ae2df801ebbc5cf0073868775bd4029526
Nathan-ecole <nathan94nahtanv2@gmail.com>
2026-03-25 11:22:15 +0100

    Changement de l'application mobile en desktop

---
+++ prestalia_desktop/android/app/src/main/kotlin/com/example/prestalia_desktop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prestalia_desktop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- prestalia_mobile/android/app/src/main/kotlin/com/example/prestalia_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.prestalia_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
